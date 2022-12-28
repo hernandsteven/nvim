68273a4be4e631d178ac9d8232bd7744e7bae7d3
hernandsteven <58962667+hernandsteven@users.noreply.github.com>
2022-12-27 20:57:51 -0500

    changed sections

--- lua/hernandsteven/plugins/lualine.lua
+++ lua/hernandsteven/plugins/lualine.lua
@@ -8,8 +8,22 @@ local lualine_theme = require("lualine.themes.nightfly")
 lualine.setup({
 	options = {
 		theme = lualine_theme,
-		component_separators = { left = "", right = "" },
+		component_separators = { left = "|", right = "|" },
 		section_separators = { left = "", right = "" },
+	},
+
+	sections = {
+		lualine_a = { "mode", "branch" },
+		lualine_b = { "" },
+		lualine_c = { "filename" },
+		lualine_d = { "filename" },
+		lualine_x = {
+			function()
+				return os.date("%I:%M %p")
+			end,
+		},
+		lualine_y = { "filetype" },
+		lualine_z = { "progress", "location" },
 	},
 
 	extensions = { "toggleterm", "nvim-tree", "fugitive" },
